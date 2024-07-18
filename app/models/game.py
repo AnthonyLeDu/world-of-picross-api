fb4ff3e48ddc584dd32b71250c04a3e8ecd77d52
AnthonyLeDu <anthony.le.du@gmail.com>
2024-07-18 21:57:10 +0200

    feat - remove some Game methods
    These methods have been moved to front-end app

--- app/models/game.py
+++ app/models/game.py
@@ -7,29 +7,6 @@ class Game(SQLModel, table=True):
     name: str
     difficulty: int
     content: list[list[bool]] | None = Field(
-        default=None, sa_column=Column(ARRAY(BOOLEAN))
+        default=None,
+        sa_column=Column(ARRAY(BOOLEAN)),
     )
-
-    @property
-    def rows_count(self) -> int:
-        return len(self.content) if self.content else 0
-
-    @property
-    def cols_count(self) -> int:
-        return len(max(self.content, key=len)) if self.content else 0
-
-    @property
-    def model_dump_preview(self) -> dict:
-        """Return model data only needed for preview: id, name, difficulty,
-        rows count and columns count.
-
-        Returns:
-            dict: Preview data.
-        """
-        return {
-            "id": self.id,
-            "name": self.name,
-            "difficulty": self.difficulty,
-            "rowsCount": self.rows_count,
-            "colsCount": self.cols_count,
-        }
